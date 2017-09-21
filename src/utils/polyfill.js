aa5ff84d17d535fd4931877571dd26bbd8462f92
Frank van der Hoek <frank.vanderhoek@gmail.com>
2017-09-21 13:42:37 +0200

    :sparkles: Allow rendering custom button

---
+++ src/utils/polyfill.js
@@ -0,0 +1,15 @@
+export default function addToBlobPolyfill() {
+  Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+    value: function(callback, type, quality) {
+      var binStr = atob(this.toDataURL(type, quality).split(',')[1]),
+        len = binStr.length,
+        arr = new Uint8Array(len);
+
+      for (var i = 0; i < len; i++) {
+        arr[i] = binStr.charCodeAt(i);
+      }
+
+      callback(new Blob([arr], { type: type || 'image/png' }));
+    }
+  });
+}
